3a955f1b14404307b4f486f8330b56f1c5302c22
sarthhh <shiva02939@gmail.com>
2023-06-18 19:58:19 +0530

    add data input endpoint and update website

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@ import express, { Express, Request, Response } from "express";
 import { Endpoints } from "./endpoints";
 import { GitaHandler } from "./handlers/gita";
 import { Database } from "./database";
-import { logger } from ".";
+
 export const app: Express = express();
 export const database = new Database();
 export const gitaHandler = new GitaHandler(database);
+
 app.use("/static", express.static("static"));
 
 app.get(Endpoints.home, (_req: Request, res: Response) => {
 	res.sendFile("static/templates/home.html", { root: "./" });
 });
+
 app.get(Endpoints.bhagavadgita, async (req: Request, res: Response) => {
 	let adhyaya: string = req.params.adhyaya;
 	let shloka: string = req.params.shloka;
