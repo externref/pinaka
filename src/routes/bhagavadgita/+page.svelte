b3a21c4ac7e12aabc3358301019fd46bb0e470a8
externref <shiva02939@gmail.com>
2023-09-06 17:42:54 +0530

    edit font size to fit screen

--- src/routes/bhagavadgita/+page.svelte
+++ src/routes/bhagavadgita/+page.svelte
@@ -19,7 +19,7 @@
 <div class="flex items-center justify-center">
     <img class="mx-auto" src="/gita_banner_text.png" alt="gita banner" width="700px">
 </div>
-<div class="mx-5 mt-9">
+<div class="mt-9">
 <i><b>"Bhagavad Gita"</b></i> which translates to "the song of God" is a part of one of the
 two well-known <i>itihasas <b>"Mahabharata"</b></i
 >. Occupying chapters 23 to 40 of the sixth book of <i>Mahabharata</i>, it is composed in
@@ -29,14 +29,15 @@ and emotional dilemma about the despair, violence, and the number of deaths this
 <i>Shri Krishna</i>, the avatar ( incarnation ) of god <i>Vishnu</i> who counsels him to
 fulfill his <i>Kshatriya Dharma</i> ( duty of a warrior ) to uphold <i>Dharma</i> through
 <i>Karma</i>.<hr class="my-3">
-<div >
+
+</div>
+<div style="font-size:small" >
     <ul style="font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Jump to Adhyaya:</ul>
     {#each adhyayaNames as adhyaya, index}
     <li>
         <a class="hover:bg-orange-500 w-max" href="bhagavadgita/{index+1}">
-            [Adhyaya {(index+1).toString().padStart(2, "0")}]: {adhyaya.at(0)} ({adhyaya.at(1)})
+            [{(index+1).toString().padStart(2, "0")}]: {adhyaya.at(0)} ({adhyaya.at(1)})
         </a> 
     </li>
     {/each}
-</div>
 </div>
